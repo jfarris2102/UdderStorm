f632876afd7fe31cb3f5d03057d25b0c3f0da149
dawsondk <dadkelly@ucsc.edu>
2015-02-26 13:30:24 -0800

    delete?

---
+++ Main/HUD.js
@@ -0,0 +1,25 @@
+
+//dadkelly
+$(document).ready(
+function(){
+    
+//canvas details
+var canvas=$("#HUD")[0];
+var ct=canvas.getContext("2d");
+var w=$("#HUD").width();
+var h=$("#HUD").height();
+//the box creation function
+window.onload=create;
+create();
+function create() {
+    //background outline
+    ct.strokeStyle="black";
+    ct.lineWidth=4;
+    ct.strokeRect(0,0,w,h);
+}
+//the draw button
+function display() {
+    
+}
+
+});
