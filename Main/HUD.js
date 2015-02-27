d1cc69b4f5b67aaecc3681327b1a9756e7b6482e
dawsondk <dadkelly@ucsc.edu>
2015-02-26 16:29:47 -0800

    Text working properly

--- Main/HUD.js
+++ Main/HUD.js
@@ -8,9 +8,16 @@ var canvas=$("#HUD")[0];
 var ct=canvas.getContext("2d");
 var w=$("#HUD").width();
 var h=$("#HUD").height();
-//the box creation function
-window.onload=create;
-create();
+//the initialize function
+window.onload=init;
+init();
+function init() {
+    if(typeof timer!="undefined") {
+        clearInterval(timer);
+    }
+    timer=setInterval(create,50);
+}
+//creating the canvas and background
 function create() {
     //background image
     var back=document.getElementById("backHUD");
@@ -21,10 +28,10 @@ function create() {
     ct.strokeStyle="black";
     ct.lineWidth=4;
     ct.strokeRect(0,0,w,h);
-    //call display function at an interval
-    setInterval(display,30);
+    //call display function
+    display();
 }
-//the draw button
+//text creation function
 function display() {
     ct.font="15px Georgia";
     ct.fillStyle="Black";
