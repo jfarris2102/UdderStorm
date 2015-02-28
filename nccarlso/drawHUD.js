660f96ed1037dc960caf3109afc0deb4aff40115
dawsondk <dadkelly@ucsc.edu>
2015-02-27 20:21:44 -0800

    Sorry

--- nccarlso/drawHUD.js
+++ nccarlso/drawHUD.js
@@ -5,7 +5,41 @@ HUD.x = 800;
 HUD.y = 0;
 HUD.image = Textures.load("images/highlight.png");
 
+function displayHUDtext(){
+	
+	//creating the text
+	var moneyD="MONEY     : "+money;
+	var foodD="FOOD      : "+food;
+	var energyD="ENERGY    : "+energy;
+	var popMarsD="POPULATION: "+popMars;
+	
+	var text1 = new TextBox(moneyD);
+	var text2 = new TextBox(foodD);
+	var text3 = new TextBox(energyD);
+	var text4 = new TextBox(popMarsD);
+	
+	text1.padTop = 25;
+	text2.padTop = 45;
+	text3.padTop = 65;
+	text4.padTop = 85;
+	
+	var textArr = [];
+	textArr.push(text1);
+	textArr.push(text2);
+	textArr.push(text3);
+	textArr.push(text4);
+	
+	for(var i = 0; i < textArr.length; i++){
+		textArr[i].font = "Courier New";
+		textArr[i].font = 15;
+		textArr[i].padLeft = 805;
+		world.addChild(textArr[i]);
+	}
+	
+}
+
 function drawHUD(){
+
     world.addChild(HUD);
 }
 
