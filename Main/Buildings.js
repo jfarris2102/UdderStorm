8d23bb8f9d5e63d21c61d79959fd060dd89dd9a4
Justin Farris <jpfarris@ucsc.edu>
2015-02-27 18:56:17 -0800

    com

---
+++ Main/Buildings.js
@@ -0,0 +1,107 @@
+//Building sprites
+var pop1  = new Sprite();
+pop1.width = 40;
+pop1.height = 50;
+pop1.visible = false;
+pop1.id = "pop1";
+pop1.sx = 1;
+pop1.sy = 1;
+pop1.image = Textures.load("images/1.png");
+
+var green1  = new Sprite();
+green1.width = 120;
+green1.height = 170;
+green1.visible = false;
+green1.id = "green1";
+green1.sx = 3;
+green1.sy = 4;
+green1.image = Textures.load("images/2.png");
+
+var solar1  = new Sprite();
+solar1.width = 40;
+solar1.height = 40;
+solar1.visible = false;
+solar1.id = "solar1";
+solar1.sx = 1;
+solar1.sy = 1;
+solar1.image = Textures.load("images/3.png");
+
+var comms1  = new Sprite();
+comms1.width = 40;
+comms1.height = 83;
+comms1.visible = false;
+comms1.id = "comms1";
+comms1.sx = 1;
+comms1.sy = 1;
+comms1.image = Textures.load("images/4.png");
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var buildings = [];
+var buildingCount = 0;
+var maxBuildings = 250;
+var buildingTypes = 4;
+
+function initBuildings(){
+	for(var i = 0; i < maxBuildings; i++){
+		var spriteTemp  = new Sprite();
+		spriteTemp.id = "null";
+		world.addChild(spriteTemp);
+		buildings[i] = spriteTemp;
+	}
+	buildingCount = 0;
+}
+
+function makeBuilding(model){
+	var temp = getModel(model);
+	buildings[buildingCount].image = temp.image;
+	buildings[buildingCount].width = temp.width;
+	buildings[buildingCount].height = temp.height;
+	buildings[buildingCount].y = sprites[HighlightX][HighlightY].y + tileSize-temp.height;
+	buildings[buildingCount].x = sprites[HighlightX][HighlightY].x - (Math.floor(temp.width/tileSize)-1)*tileSize;
+	buildings[buildingCount].id = temp.id;
+}
+
+function makeGhost(model){
+	var temp = getModel(model);
+	placing.image = temp.image;
+	placing.width = temp.width;
+	placing.height = temp.height;
+}
+
+function getModel(model){
+	switch(model) {
+    case 1:
+        return pop1;
+        break;
+    case 2:
+        return green1;
+        break;
+    case 3:
+        return solar1;
+        break;
+    case 4:
+        return comms1;
+        break;
+    default:
+        return pop1;
+		break;
+	}
+}
+
+function BuildingSize(model){
+	var building = getModel(model);
+	var temp = {};
+	temp.sx = building.sx;
+	temp.sy = building.sy;
+	return temp;
+}
+
+function checkOccupied(model, xOff, yOff){
+	for(var i = 0; i < BuildingSize(model).sy; i++){
+		for(var j = 0; j < BuildingSize(model).sx; j++){
+			if(tileGrid[xOff-j][yOff-i].occupied == true) return true;
+		}
+	}
+	return false;
+}
---
+++ Main/Mars.js
@@ -0,0 +1,306 @@
+//This  program creates a tile engine
+use2D = true;
+var marsActive = false;
+function startMars(){
+	marsActive = true;
+	drawTileEngine();
+	canvas.addEventListener("mousemove", drawTileEngine, false);
+	//manager for sprite dragging
+	world.addChild(manager2);
+	gInput.addMouseDownListener(manager2);
+	gInput.addMouseUpListener(manager2);
+}
+
+function stopMars(){
+	marsActive = false;
+	canvas.removeEventListener("mousemove", drawTileEngine)
+	gInput.removeMouseDownListener(manager2);
+	gInput.removeMouseUpListener(manager2);
+	world.removeChild(manager2);
+}
+
+function newGameMars(){
+	for ( i=0; i< tileGrid.length; i++ ) {
+		for ( j = 0; j< tileGrid[i].length; j++ ) {
+			tileGrid[i][j].occupied =false;
+		}
+	}
+	for(var i = 0; i < buildingCount; i++){
+		console.log(i);
+		world.removeChild(buildings[i]);
+	}
+	placeBuildingMode = false;
+	flip = false;
+	flop = false;
+	myX = worldSize/2;
+	myY = worldSize/2;
+	selection = 1;
+	highlight.alpha = 0;
+	placing.alpha = 0;
+	initBuildings();
+}
+
+//dimension variables(easy to edit)
+var worldSize=12000;
+var tileSize=40;
+//variable holds the # of tiles per line
+var tilesPerLine= Math.floor(worldSize/tileSize);
+//Variables for the display functions
+//should be how big the canvas is
+var drawHeight=640;
+var drawWidth=960;
+//holds the displayed number visual tiles
+var drawTileWidth = Math.floor(drawWidth/tileSize);
+var drawTileHeight = Math.floor(drawHeight/tileSize);
+
+//Sprite stuff
+var sandSprite  = new Sprite();
+sandSprite.width = tileSize;
+sandSprite.height = tileSize;
+sandSprite.visible = false;
+sandSprite.image = Textures.load("images/sandSprite.png");
+
+var rockSprite  = new Sprite();
+rockSprite.width = tileSize;
+rockSprite.height = tileSize;
+rockSprite.visible = false;
+rockSprite.image = Textures.load("images/rockSprite.png");
+
+var sprites = [];
+for(var i = 0; i < Math.floor(960/tileSize)+2; i++){
+	var cols = [];
+	for(var j = 0; j < Math.floor(640/tileSize)+2; j++){
+		var spriteTemp  = new Sprite();
+		spriteTemp.width = tileSize;
+		spriteTemp.height = tileSize;
+		spriteTemp.x = 0;
+		spriteTemp.y = 0;
+		world.addChild(spriteTemp);
+		cols[j] = spriteTemp;
+	}
+	sprites[i] = cols;
+}
+
+//Mouse movement stuff
+var manager2 = new Sprite();
+var dragging = false;
+var placeBuilding = false;
+var placeBuildingMode = false;
+var flip = false;
+var flop = false;
+var selection = 1;
+var MouseCurrX = 0;
+var MouseCurrY = 0;
+var MousePrevX = 0;
+var MousePrevY = 0;
+var MouseOverFirst = true;
+
+var highlight  = new Sprite();
+highlight.width = tileSize;
+highlight.height = tileSize;
+highlight.alpha = 0;
+highlight.image = Textures.load("images/highlight.png");
+world.addChild(highlight);
+
+//constants hold the ids for grid squares
+var sandTile=0;
+var rockTile=1;
+//creates an array with the respective colors for each grid square
+var tileColors= ['#0000DD','#00CC00'];
+var totalTileTypes=2;
+//these hold the center of the grid and also the center of the draw grid
+var myX = worldSize/2;
+var myY = worldSize/2;
+//Previous
+var myXP = myX;
+var myYP = myY;
+//this array holds a worldsize/tilesize square array of ints 0 through totalTileTypes-1
+var tileGrid = [];
+var tiles = [];
+//assigns a value 0 through totalTileTypes-1 to each grid square
+for ( i=0; i<tilesPerLine; i++ ) {
+    var column = new Array();
+    //creates an array of ints worldsize/tilesize long
+    for ( j = 0; j<tilesPerLine; j++ ) {
+		var temp = {};
+        temp.type = Math.floor (Math.random() * totalTileTypes);
+		temp.occupied = false; 
+		column[j] = temp;
+    }
+    //assigns each column to a row in the main 2D array
+    tileGrid[i] = column;
+}
+
+//A
+gInput.addBool(65, "left");
+//D
+gInput.addBool(68, "right");
+//S
+gInput.addBool(83, "down");
+//Esc
+gInput.addBool(27, "escape");
+
+////////////////////////////////////////////////// Functions /////////////////////////////////////////////////////////////
+
+window.onkeypress = function(event) { //Global Esc key functionality
+	if(marsActive){
+		if(gInput.left && placeBuildingMode){
+			selection = Math.max(1, --selection);
+			flip = true;
+		}else if(gInput.right && placeBuildingMode){
+			selection = Math.min(buildingTypes, ++selection);
+			flip = true;
+		}else if(gInput.down){
+			if(placeBuildingMode){
+				placeBuildingMode = false;
+				flip = false;
+				flop = true;
+			}
+			else{
+				placeBuildingMode = true;
+				flip = true;
+				flop = false;
+			}
+		}else if(gInput.escape){ //Back to menu
+			stopMars();
+			start();
+		}
+		drawTileEngine();
+	}else if(tutorialActive){
+		if(gInput.escape){
+		world.removeChild(TutorialPage);
+		tutorialActive = false;
+		start();
+		}
+	}
+}
+
+//Check if sprite clicked
+manager2.onMouseDown = function () {
+	dragging = true;
+}
+manager2.onMouseUp = function () {
+	placeBuilding = true;
+	dragging = false;
+	MouseOverFirst = true;
+	drawTileEngine();
+}
+
+function sortBuildings(){
+	var temp;
+	for(var i = 0; i < buildingCount-1; i++){
+		for(var j = 1; j < buildingCount-i; j++){
+			if(buildings[j-1].y+buildings[j-1].height > buildings[j].y+buildings[j].height){
+				temp = buildings[j-1];
+				buildings[j-1]= buildings[j];
+				buildings[j] = temp;
+			}
+		}
+	}
+	for(var i = 0; i < buildingCount; i++){
+		world.removeChild(buildings[i]);
+		world.addChild(buildings[i]);
+	}
+}
+
+var placing  = new Sprite();
+placing.alpha = 0.3;
+world.addChild(placing);
+
+//setInterval ( drawTileEngine, 1000 / 30 ); //30 FPS
+//set up this method so that the center of the grid follows the mouse pointer if the mouse is held down(draggingging)
+function drawTileEngine() {
+	var model = selection;
+	//these vars hold the coordinates of the upper left 
+	//corner of the screen
+	var leftSide= myX - drawWidth/2; 
+	var topSide= myY - drawHeight/2;
+	//these vars hold how much of the left and top tiles are drawn   
+	var leftTile = Math.floor( leftSide/ tileSize);
+	var topTile = Math.floor( topSide/ tileSize);
+	//these vars hold the tile at the top left's x and y 
+	var tileOffsetX = leftSide % tileSize;
+	var tileOffsetY = topSide % tileSize;
+	//draws every tile visible on the grid with appropriate offsets
+	for ( x = 0; x < drawTileWidth+2; x++ ) {
+		for ( y = 0; y < drawTileHeight+2; y++ ) {
+			var tileColor = tileGrid[leftTile+x][topTile+y].type;
+			if(tileColor == 0){
+				sprites[x][y].image = sandSprite.image;
+			}else if(tileColor == 1){
+				sprites[x][y].image =  rockSprite.image;
+			}else{
+				alert("Invalid texture code");
+			}
+			sprites[x][y].x = (x-1) * tileSize - tileOffsetX;
+			sprites[x][y].y = (y-1) *  tileSize - tileOffsetY;
+		}
+	}
+	//Update building positions
+	for(var i = 0; i < buildingCount; i++){
+		buildings[i].x -= myX-myXP;
+		buildings[i].y -= myY-myYP;
+	}
+	myXP = myX;
+	myYP = myY;
+	if(MouseOverFirst){
+		MouseOverFirst = false;
+		MousePrevX = gInput.mouse.x;
+		MousePrevY = gInput.mouse.y;
+	}
+	MouseCurrX = gInput.mouse.x;
+	MouseCurrY = gInput.mouse.y;
+	//Get Tile that Mouse is Over and highlight
+	HighlightX = Math.ceil((MouseCurrX+tileOffsetX)/tileSize);
+	HighlightY = Math.ceil((MouseCurrY+tileOffsetY)/tileSize);
+	if(HighlightX > drawWidth/tileSize+1) HighlightX = drawWidth/tileSize+1;
+	else if(HighlightX < 1) HighlightX = 1;
+	if(HighlightY > drawHeight/tileSize+1) HighlightY = drawHeight/tileSize+1;
+	else if(HighlightY < 1) HighlightY = 1;
+	if(flip){
+		makeGhost(model);
+		placing.alpha = 0.5;
+		highlight.alpha = 0.5;
+		flip = false;
+	}else if(flop){
+		placing.alpha = 0;
+		highlight.alpha = 0;
+	}
+	if(placeBuildingMode){
+		highlight.width = BuildingSize(model).sx*tileSize;
+		highlight.height = BuildingSize(model).sy*tileSize;
+		highlight.x = sprites[HighlightX][HighlightY].x-highlight.width+tileSize;
+		highlight.y = sprites[HighlightX][HighlightY].y-highlight.height+tileSize;
+		placing.x = sprites[HighlightX][HighlightY].x - (Math.floor(placing.width/tileSize)-1)*tileSize;
+		placing.y = sprites[HighlightX][HighlightY].y + tileSize-placing.height;
+	}
+	//Check if occupied by building
+	if(!checkOccupied(model, leftTile+HighlightX, topTile+HighlightY)){
+		if(placeBuilding && placeBuildingMode && buildingCount < maxBuildings){ //Places a building
+			makeBuilding(model); //in Building.js
+			buildingCount++;
+			sortBuildings();
+			for(var i = 0; i < BuildingSize(model).sy; i++){
+				for(var j = 0; j < BuildingSize(model).sx; j++){
+					tileGrid[leftTile+HighlightX-j][topTile+HighlightY-i].occupied = true;
+				}
+			}
+			highlight.image =  Textures.load("images/highlight2.png");
+		}else highlight.image =  Textures.load("images/highlight.png");
+	}else highlight.image =  Textures.load("images/highlight2.png");
+	if(dragging){
+		myX -= MouseCurrX-MousePrevX;
+		myY -= MouseCurrY-MousePrevY;
+		//edge of screen
+		if(myX < drawWidth/2) myX = drawWidth/2;
+		else if(myX > worldSize-(drawWidth)-tileSize*2) myX = worldSize-(drawWidth)-tileSize*2;
+		if(myY < drawHeight/2) myY = drawHeight/2;
+		else if(myY > worldSize-(drawHeight/2)-tileSize*2) myY = worldSize-(drawHeight/2)-tileSize*2;
+	}
+	placeBuilding = false;
+	//Set previous mouse pos
+	MousePrevX = MouseCurrX;
+	MousePrevY = MouseCurrY;
+}//end of drawTileEngine()
+
+
