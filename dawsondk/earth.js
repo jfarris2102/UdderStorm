fdf66143d575be07428fde2007a56e114ff1d823
dawsondk <dadkelly@ucsc.edu>
2015-03-03 20:35:44 -0800

    Border

--- dawsondk/earth.js
+++ dawsondk/earth.js
@@ -11,5 +11,12 @@ var bg=new Sprite(); {
     bg.y=0;
     bg.image=Textures.load(back);
 } world.addChild(bg);
-//
-
+//load border
+var border="images/Border.png";
+var bd=new Sprite(); {
+    bd.width=970;
+    bd.height=650;
+    bd.x=-5;
+    bd.y=-5;
+    bd.image=Textures.load(border);
+} world.addChild(bd);
