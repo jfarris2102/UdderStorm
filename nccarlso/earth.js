7bfb84ffcebd1f8eb516f291a87961df4ec7d587
Justin Farris <jpfarris@ucsc.edu>
2015-03-05 13:14:45 -0800

    c

--- nccarlso/earth.js
+++ nccarlso/earth.js
@@ -20,3 +20,17 @@ var bd=new Sprite(); {
     bd.y=-5;
     bd.image=Textures.load(border);
 } world.addChild(bd);
+function getLifetime(){
+	var ePerCapita = 2000*energyFactor; //2,000 kg of oil equivalent per capita at 2015
+	var envDamage = 2*damageFactor;
+	//At this environmental impact, human civilization collapses
+	var maxImpact = 55;
+	//Impact on environment = Population * Energy Consumption per capita * Environ.Damage from E.C.
+	var impact = (popEarth * ePerCapita * envDamage)/1000000000000;
+	var lifetime = Math.log(maxImpact/impact) / Math.log(1+growthRate);
+	//After about 500 million years Earth will no longer be in habitable zone 
+	//of the solar system (for humans)
+	if(lifetime > 500000000);
+	   lifetime = 500000000;
+	return lifetime;
+}
