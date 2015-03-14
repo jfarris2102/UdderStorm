df61600848e573ada5b7136d5f613852d0078916
Justin Farris <jpfarris@ucsc.edu>
2015-03-14 00:46:55 -0700

    c

--- nccarlso/javascript/Buildings.js
+++ nccarlso/javascript/Buildings.js
@@ -453,6 +453,7 @@ function updateResource(){
 	if(atmosphere>100) atmosphere = 100;
 	temperature += tempInc;
 	if(temperature>60) temperature = 60;
+	researchPoints = resInc;
 }
 
 //resets all of the stored variables to their default values
